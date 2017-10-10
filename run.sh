5c60c2575848841df8656b1d48aa9af75252cbed
And Grin <a@grin.ly>
2017-10-09 20:38:07 -0700

    more flexible run

--- run.sh
+++ run.sh
@@ -9,10 +9,9 @@ docker build -t $SVC . || exit 1
 
 docker run --rm -it \
 	-p $PORT:$PORT \
-	-p 11000:11000 \
     --name $SVC \
     -e PORT=$PORT \
     -e GOODSYNC_USER=$1\
     -e GOODSYNC_PWD=$2\
     -v $(pwd)/data:/data \
-    $SVC
+    $SVC $3
